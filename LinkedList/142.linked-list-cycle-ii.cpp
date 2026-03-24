c400805ea9976c0af4850e1460bf040f951f5f1e
Pushpendu <myselfabhi2002@gmail.com>
2026-03-24 07:54:17 +0000

    Solution

---
+++ LinkedList/142.linked-list-cycle-ii.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=142 lang=cpp
+ *
+ * [142] Linked List Cycle II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow=head;
+        ListNode *fast=head;
+        ListNode *temp=head;
+       
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                while(temp!=slow){
+                    temp=temp->next;
+                    slow=slow->next;
+                }
+                return temp;
+            }
+        }
+        return NULL;
+    }
+};
+// @lc code=end
+
