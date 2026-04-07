c4735c38adbfc90976b490f27f82359102327b63
Pushpendu <myselfabhi2002@gmail.com>
2026-04-07 18:36:40 +0000

    Added Solution

---
+++ Array/162.find-peak-element.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=162 lang=cpp
+ *
+ * [162] Find Peak Element
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+
+        int left=1;
+        int right=n-2;
+        int ans=-1;
+        while(left<=right){
+            int mid=(right+left)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                ans=mid;
+                break;
+            }else if(nums[mid]<nums[mid-1] && nums[mid]>nums[mid+1]){
+                right=mid-1;
+            }else if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1]){
+                left=mid+1;
+            }else {
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
